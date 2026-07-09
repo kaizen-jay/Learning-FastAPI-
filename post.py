90c2b6396e415fcf1186a5aa632b04815faa3ccd
Jay Raj <haldarjayraj@gmail.com>
2026-07-09 13:36:56 +0530

    Updates

--- post.py
+++ post.py
@@ -1,1 +1,5 @@
-#Step 1: Ham pehle bhi client se server pe http request bhej rahe the but wo 'get' type me thi matlab ham data retrieve kar rahe the. In this step we will send an HTTP request in the 'post' jisse ki ham data server pe bhejenge.
+#Step 1: Ham pehle bhi client se server pe http request bhej rahe the but wo 'get' type me thi matlab ham data retrieve kar rahe the. In this step we will send an HTTP request in the 'post' jisse ki ham data server pe bhejenge like creating or updating data on the server.
+#Ye jo file ham bhej rahe hai in the json format to the server is called the request body and the definition is in the notes
+
+print("I am travelling so I might not get time to code today so heres the green tick for one contribution 😭")
+
