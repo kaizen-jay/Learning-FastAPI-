e7a2f79c07b412d866fa49883652578fda997b22
Jay Raj <haldarjayraj@gmail.com>
2026-07-18 09:41:10 +0530

    initialized the delete endpoint

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+'''Now we will make the delete endpoint and it will only take user id. and ye patient ka id ham url me as a path parameter provdie karenge. HTTP method that we will use is also DELETE.
+# The flow is - Patient id le rhe hai , usme data load kar rahe hai, then uske andar se vo key-value pair hata de rahe hai jiski key patient_id hai'''
+
+# @app.delete('/delete/{patient_id}')
+# def delete_patient(patient_id: str) #ek function bana lenge 
