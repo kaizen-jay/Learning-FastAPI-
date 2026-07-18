f5d3fecd84b8a3b7fd3f81c4af29a7d1ff0dd9a6
Jay Raj <haldarjayraj@gmail.com>
2026-07-18 23:53:28 +0530

    update

--- main.py
+++ main.py
@@ -221,4 +221,3 @@ def delete_patient(patient_id: str): #ek function bana lenge jisko patient_id as
 
     return JSONResponse(status_code=200, content={'message': 'patient deleted'})
 
-#making it green
